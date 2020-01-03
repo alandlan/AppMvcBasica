54f4c53c6996b6543ace630ed49021fff7c6b87d
Alan Vieira Martins <alanhorcrux@gmail.com>
2020-01-02 21:22:02 -0300

    Criado migration das tabelas

--- AppMvcBasica/Models/Endereco.cs
+++ AppMvcBasica/Models/Endereco.cs
@@ -1,11 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppMvcBasica.Models
 {
     public class Endereco : Entity
     {
+        public Guid FornecedorId { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(200, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Logradouro { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(50, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(8, ErrorMessage = "O Campo {0} precisa ter {1}", MinimumLength = 8)]
+        public string Cep { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Bairro { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(100, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Cidade { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(50, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Estado { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
     }
 }
--- AppMvcBasica/Models/Produto.cs
+++ AppMvcBasica/Models/Produto.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace AppMvcBasica.Models
 {
@@ -9,11 +7,25 @@ namespace AppMvcBasica.Models
     {
         public Guid FornecedorId { get; set; }
 
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(200, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(1000, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Descricao { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
+        [StringLength(200, ErrorMessage = "O Campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
         public string Imagem { get; set; }
+
+        [Required(ErrorMessage = "O Campo {0} é Obrigatório")]
         public decimal Valor { get; set; }
+
         public DateTime DataCadastro { get; set; }
+
         public bool Ativo { get; set; }
+
+        public Fornecedor Fornecedor { get; set; }
     }
 }
